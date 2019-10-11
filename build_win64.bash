d1a2006f32b2659f179fd19875d13c08f70796f0
Kikyou Akino <bellflower@web4u.jp>
2019-10-11 03:29:58 +0000

    Add build_win64.bash

---
+++ build_win64.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DEPENDS=depends/x86_64-w64-mingw32
+
+make -C depends HOST=x86_64-w64-mingw32 -j$(nproc) && \
+    $DEPENDS/native/bin/qmake \
+    USE_BUILD_INFO=1 \
+    BOOST_LIB_SUFFIX=-mt-s \
+    BOOST_THREAD_LIB_SUFFIX=-mt-s \
+    BOOST_INCLUDE_PATH=$DEPENDS/include/boost \
+    BOOST_LIB_PATH=$DEPENDS/lib \
+    OPENSSL_INCLUDE_PATH=$DEPENDS/include/openssl \
+    OPENSSL_LIB_PATH=$DEPENDS/lib \
+    BDB_INCLUDE_PATH=$DEPENDS/include \
+    BDB_LIB_PATH=$DEPENDS/lib \
+    MINIUPNPC_INCLUDE_PATH=$DEPENDS/include \
+    MINIUPNPC_LIB_PATH=$DEPENDS/lib \
+    QMAKE_LRELEASE=$DEPENDS/native/bin/lrelease \
+    SanDeGo-qt.pro && \
+    make -j$(nproc) -f Makefile.Release && \
+    x86_64-w64-mingw32-strip release/*.exe
