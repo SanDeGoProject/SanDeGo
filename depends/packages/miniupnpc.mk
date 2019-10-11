f63d89addfdbd7cf72b3c800393fa1d977ca9346
Kikyou Akino <bellflower@web4u.jp>
2019-10-11 10:27:29 +0900

    Update depends packages
     * boost
     * miniupnpc
     * openssl
     * qt with qtbase

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -1,8 +1,8 @@
 package=miniupnpc
-$(package)_version=2.0.20180203
-$(package)_download_path=http://miniupnp.free.fr/files
+$(package)_version=2.1.20190408
+$(package)_download_path=https://miniupnp.tuxfamily.org/files
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=90dda8c7563ca6cd4a83e23b3c66dbbea89603a1675bfdb852897c2c9cc220b7
+$(package)_sha256_hash=a0c46bcf6065d6351a8fa6a0a18dc57d10a16908dbb470908fd2e423511514ec
 
 define $(package)_set_vars
 $(package)_build_opts=CC="$($(package)_cc)"
